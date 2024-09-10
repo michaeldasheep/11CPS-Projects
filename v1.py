15b5dfc902b58bb95278937b63c8c9ccbd29f1a6
Michael Zhao <michael@michaeldasheep.com>
2024-09-10 16:28:20 +1200

    finished data entry/task 1

--- v1.py
+++ v1.py
@@ -8,10 +8,10 @@ def menu():
         userInput = input("Type 1 to input data, Type 2 to search the data, Type 3 to read the data, Type e to exit: \n").lower()
         if userInput == "1":
             inputData()
-        elif userInput == "2":
-            searchData()
-        elif userInput == "3":
-            readData()
+#        elif userInput == "2":
+#            searchData()
+#        elif userInput == "3":
+#            readData()
         elif userInput == "e":
             cond = False
         else:
@@ -39,22 +39,47 @@ def inputData():
                             condition = False
                         else:
                             print("Please input a valid number!")
-        file = open("data.json", "w")
-        fileContents = json.dumps(data)
-        file.write(fileContents)
-        file.close()
-        return True
+        inputWriteToFile(data)
 
 def batchInputData():
+    data = {}
     buses = ["A","B","C","D","E","F"]
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     weeks = [1, 2, 3, 4]
     for bus in buses:
-        batchUserInput = input("Input the data for the on time performance of the bus for days in format num1,num2,num3,...etc with them being in order of day from Monday1 - Sunday1, Monday2 - Sunday2, etc: \n")
-        batchUserArray = batchUserInput.split(",")
         condition = True
         while condition == True:
-            batchUserNum = len(batchUserArray)
+            batchUserInput = input(f"Input the data for the on time performance of the bus {bus} for days in format num1,num2,num3,...etc with them being in order of day from Monday1 - Sunday1, Monday2 - Sunday2, etc: \n")
+            batchUserArray = batchUserInput.split(",")
+            if len(batchUserArray) == 20:
+                nd = 0
+                nw = 0
+                for x in batchUserArray:
+                    if nw < 6:
+                        currentDay = days[nd]
+                        currentWeek = weeks[nw]
+                        append = int(x)
+                        data[f'{bus}{currentWeek}{currentDay}'] = append
+                        if nd == 4:
+                            nw = nw + 1
+                            nd = 0
+                        else:
+                            nd = nd + 1
+                    else:
+                        pass
+                condition = False
+            else:
+                print("INPUT A CORRECT SET OF DATA PLEASE")
+    inputWriteToFile(data)
+
+def inputWriteToFile(data: dict):
+    file = open("data.json", "w")
+    fileContents = json.dumps(data)
+    file.write(fileContents)
+    file.close()
+    print("Successfully written data to file!\n")
+    return True
 
 if __name__ == "__main__":
     menu()
+    exit()
